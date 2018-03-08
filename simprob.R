5266edcd0ffcc4cab1203b44ec7ba1136ba8f7b2
ShanA7 <33241525+ShanA7@users.noreply.github.com>
2018-03-08 21:15:12 +1100

    add formula and lots of stuff

--- simprob.R
+++ simprob.R
@@ -7,6 +7,13 @@ library(lattice)
 #check the sample
 coin <- c(0,1)
 result <- sample(coin,10, replace = TRUE)
-#hist(result)
+result
 histogram(result)
+
+barchart(result)
 densityplot(result)
+plot(result)
+hist(result)
+stripplot(result)
+barchart(table(result), horizontal = FALSE)
+summary(result)
