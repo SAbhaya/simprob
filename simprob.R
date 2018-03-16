8b94bcaa18d5e894c5053309b1f01ef518693630
ShanA7 <33241525+ShanA7@users.noreply.github.com>
2018-03-16 23:47:57 +1100

    add syntex using dplyr and tidyr libraries.

--- simprob.R
+++ simprob.R
@@ -1,14 +1,21 @@
 #load lattice
 
 library(lattice)
-
-
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 
 #check the sample
 coin <- c(0,1)
 result <- sample(coin,10, replace = TRUE)
 result
 histogram(result)
+
+dfresult <- data.frame(result)
+recount <- count(dfresult,result)
+byresult <- spread(recount,result,n)
+names(byresult) = c("TAIL", "HEAD")
+mutate(byresult, pretail = TAIL/(TAIL+ HEAD))
 
 barchart(result)
 densityplot(result)
