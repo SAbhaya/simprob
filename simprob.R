c5b5ffc41cf185b943695cb78ee8abbfab26fb67
Sudarshana A <33241525+ShanA7@users.noreply.github.com>
2018-03-29 16:08:17 +1100

    add lots of stuff

--- simprob.R
+++ simprob.R
@@ -4,11 +4,23 @@ library(lattice)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(gridExtra)
 
 #check the sample
-coin <- c(0,1)
+coin <- c("H","T")
 result <- sample(coin,10, replace = TRUE)
-df <- data.frame(result)
+#df <- data.frame(result)
+df_total <- data.frame(result) %>% count(result)
+ggplot(df_total, aes(result,n, fill = result)) + geom_bar(stat = "identity")
+df_prob <- spread(df_total,result,n) %>% 
+  mutate(prob_H = H/(H+T) , prob_T = T/(H+T) ) %>% 
+  select(prob_H, prob_T) %>% gather(event,prob,prob_H:prob_T)
+
+ggplot(df_prob, aes(event,prob, fill = event)) + geom_bar(stat = "identity")
+
+
+#######################new code above
+
 #names(df) <- c('T', 'H')
 df <- spread(count(df,result),result,n)
 names(df) <- c('T', 'H')
